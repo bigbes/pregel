00525237c1ecbdaf6b5414e889039ddff0b7d545
bigbes <bigbes@gmail.com>
2016-05-18 14:25:40 +0300

    Unify config for master/slave

--- test/test-multi/test-worker.lua
+++ test/test-multi/test-worker.lua
@@ -1,35 +1,92 @@
-local common = require('common')
-local worker = require('pregel.worker')
+local ploader = require('pregel.loader')
+local pmaster = require('pregel.master')
+local pworker = require('pregel.worker')
 
 local xpcall_tb = require('pregel.utils').xpcall_tb
 
-local port_offset = arg[0]:match('(%d+)')
+local worker, port_offset = arg[0]:match('(%a+)-(%d+)')
 assert(port_offset ~= nil, 'bad worker name')
+
+port_offset = port_offset or 0
 
 box.cfg{
     wal_mode = 'none',
-    listen = 'localhost:' .. tostring(3302 + port_offset),
+    listen = 'localhost:' .. tostring(3301 + port_offset),
     background = true
+    logger_nonblock = false
 }
 
 box.schema.user.grant('guest', 'read,write,execute', 'universe', nil, {
     if_not_exists = true
 })
 
-local worker = worker.new('test', {
-    master      = 'localhost:3301',
-    workers     = {
+local function inform_neighbors(self, val)
+    for id, neighbor, weight in self:pairs_edges() do
+        self:send_message(neighbor, val)
+    end
+end
+
+local function graph_max_process(self)
+    if self.superstep == 1 then
+        inform_neighbors(self, self:get_value())
+    elseif self.superstep < 30 then
+        local modified = false
+        for _, msg in self:pairs_messages() do
+            if self:get_value() < msg then
+                self:set_value(msg)
+                modified = true
+            end
+        end
+        if modified then
+            inform_neighbors(self, self:get_value())
+        end
+    end
+    self:vote_halt(true)
+end
+
+local common_cfg = {
+    master       = 'localhost:3301',
+    workers      = {
+        'localhost:3302',
         'localhost:3303',
         'localhost:3304',
         'localhost:3305',
-        'localhost:3306',
     },
-    compute     = common.graph_max_process,
-    combiner    = math.max,
-    preload     = common.preload_from_file('../data/soc-Epinions1.txt'),
-    squash_only = false
-})
+    compute      = graph_max_process,
+    combiner     = math.max,
+    preload      = ploader.graph_edges_f,
+    preload_args = '../data/soc-Epinions-custom-bi.txt',
+    -- preload      = ploader.graph_edges_cf,
+    -- preload_args = '/Users/bigbes/src/work/pregel-data/actual/soc-pokec-relationshit-custom-bi.txt',
+    squash_only  = false
+}
 
--- worker:add_agregator('information', {
---     ''
--- })
+if worker == 'worker' then
+    box.cfg{
+        wal_mode = 'none',
+        listen = 'localhost:' .. tostring(3301 + port_offset),
+        background = true
+        logger_nonblock = false
+    }
+else
+    box.cfg{
+        wal_mode = 'none',
+        listen = 'localhost:' .. tostring(3301 + port_offset),
+        logger_nonblock = false
+    }
+end
+
+box.schema.user.grant('guest', 'read,write,execute', 'universe')
+
+if worker == 'worker' then
+    worker = pworker.new('test', common_cfg)
+else
+    xpcall_tb(function()
+        local master = pmaster.new('test', common_cfg)
+        master:wait_up()
+        master:preload()
+        master.mpool:send_wait('snapshot')
+        master:start()
+    end)
+    os.exit(0)
+end
